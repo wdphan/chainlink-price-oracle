97b77df3c1ad81eee13ac692649e72140d85f45c
William Phan <williamphan@MacBook-Pro-10.fios-router.home>
2022-10-09 20:54:07 -0400

    updated contract

--- src/ETH_USD.sol
+++ src/ETH_USD.sol
@@ -1,40 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
 
-// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract ChainlinkPriceOracle {
+contract PriceConsumerV3 {
     AggregatorV3Interface internal priceFeed;
 
+    /**
+     * Network: Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+     */
     constructor() {
-        // ETH / USD
         priceFeed = AggregatorV3Interface(
-            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
         );
     }
 
+    /**
+     * Returns the latest price
+     */
     function getLatestPrice() public view returns (int256) {
         (
-            uint80 roundID,
+            ,
+            /*uint80 roundID*/
             int256 price,
-            uint256 startedAt,
-            uint256 timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        // for ETH / USD price is scaled up by 10 ** 8
-        return price / 1e8;
-    }
-}
+            ,
+            ,
 
-interface AggregatorV3Interface {
-    function latestRoundData()
-        external
-        view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
+        ) = /*uint startedAt*/
+            /*uint timeStamp*/
+            /*uint80 answeredInRound*/
+            priceFeed.latestRoundData();
+        return price;
+    }
 }
