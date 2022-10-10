2535b50bb194a9ddc427c7d6e9e1b1569222343f
William Phan <williamphan@MacBook-Pro-10.fios-router.home>
2022-10-09 20:37:08 -0400

    added price oracle

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,40 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-contract Contract {
-    string public greet = "Hello World!";
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract ChainlinkPriceOracle {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        // ETH / USD
+        priceFeed = AggregatorV3Interface(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        // for ETH / USD price is scaled up by 10 ** 8
+        return price / 1e8;
+    }
+}
+
+interface AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
 }
